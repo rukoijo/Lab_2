2c65a9cee7cd525869c13d9f7c98e5aa06f0f0d8
Amani <99120845+rukoijo@users.noreply.github.com>
2023-10-20 06:47:04 +0200

    API fixed GET, PUT, POST, DELETE methods working

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const connection = require("./connection");
 const Recipe = require("./recipe"); 
 const app = express();
+exports.app = app;
 require("dotenv").config();
 
 connection();
 app.use(express.static('public'));
 app.use(express.json());
 
+//this fetches all the recipes from the database
 app.get('/recipes', async (req, res) => { 
     try {
         const recipes = await Recipe.find(); 
@@ -19,49 +21,49 @@ app.get('/recipes', async (req, res) => {
     }
 });
 
-// app.post('/recipe', (req, res) => { 
-//     const data = new Recipe(req.body); 
-//     data.save()
-//         .then(recipe => { 
-//             console.log('Recipe Added!', recipe);
-//             res.json({ success: true, recipe });
-//         })
-//         .catch(err => console.log(err));
-// });
+//this fetches a specific recipe from the database based on the recipe id
+app.get('/recipes/:recipeId', async (req, res) => {
+    try {
+        const recipeId = req.params.recipeId;
+        const recipe = await Recipe.findById(recipeId);
+        console.log("GET RECIPE", recipe);
+        if (!recipe) {
+            return res.status(404).json({ error: 'Recipe not found' });
+        }
+        res.status(200).json(recipe);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
-// app.delete('/recipe/:recipeId', async (req, res) => { 
-//     try {
-//         const recipeId = req.params.recipeId; 
-//         const deletedRecipe = await Recipe.findByIdAndDelete(recipeId); 
+//this adds a recipe to the database
+app.post('/recipes', (req, res) => { 
+    const data = new Recipe(req.body); 
+    data.save()
+        .then(recipe => { 
+            console.log('Recipe Added!', recipe);
+            res.json({ success: true, recipe });
+        })
+        .catch(err => console.log(err));
+});
 
-//         if (!deletedRecipe) {
-//             return res.status(404).json({ error: 'Recipe Not found' });
-//         }
-//         res.json({ success: true, deletedRecipe });
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+//this updates a recipe in the database based on the recipe id
+app.put('/recipes/:recipeId', async (req, res) => { 
+    try {
+        const recipeId = req.params.recipeId;
+        const updatedData = req.body;
+        const updatedRecipe = await Recipe.findByIdAndUpdate(recipeId, updatedData, { new: true });
+        if (!updatedRecipe) {
+            return res.status(404).json({ error: 'Recipe not found' });
+        }
+        res.status(200).json(updatedRecipe);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
-// app.put('/recipe/:recipeId', async (req, res) => { 
-//     try {
-//         const recipeId = req.params.recipeId;
-//         const updatedData = req.body;
-
-//         const updatedRecipe = await Recipe.findByIdAndUpdate(recipeId, updatedData, { new: true });
-
-//         if (!updatedRecipe) {
-//             return res.status(404).json({ error: 'Recipe not found' });
-//         }
-
-//         res.status(200).json(updatedRecipe);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
-
+// Sends the 'home.html' file as a default response to a call to the root URL
 app.get('/', (req, res) => {
-    // Send the 'home.html' file as a response
     res.sendFile(__dirname + '/home.html');
   });
 
